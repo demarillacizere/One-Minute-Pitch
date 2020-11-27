013503b011e3f392f3a86396e9fce9c4872b8d05
Demarillac Izere <izered3@gmail.com>
2020-11-27 07:19:04 +0200

    added models

--- app/models.py
+++ app/models.py
@@ -1,1 +1,80 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class Pitch(db.model):
+    '''
+    Movie class to define Movie Objects
+    '''
+
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer,primary_key = True)
+    content = db.Column(db.String)
+    category = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    comments = db.relationship('Comment',backref='pitch',lazy='dynamic')
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_pitches(cls,category):
+        pitches = Pitch.query.filter_by(pitch_category=category).all()
+        return pitches
+
+
+class Comment(db.Model):
+
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    pitch_id = db.Column(db.Integer,db.ForeignKey("pitches.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,id):
+        comments = Comment.query.filter_by(pitch_id=id).all()
+        return comments
+
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
+    pitches = db.relationship('Pitch',backref = 'user',lazy = "dynamic")
+    comments = db.relationship('Comment',backref = 'user',lazy = "dynamic")
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_secure,password)
+
+    def __repr__(self):
+        return f'User {self.username}'        
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-
+from  flask_migrate import Migrate, MigrateCommand
 
 app = create_app('development')
 
@@ -8,6 +8,8 @@ manager = Manager(app)
 manager.add_command('server',Server)
 
 manager.add_command('server',Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
